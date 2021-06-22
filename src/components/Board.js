28f698d5bce521638cbc896ed768ee1337f13758
Mia Chen <mia.chen.mtl@gmail.com>
2021-06-21 23:05:31 -0400

    include milligram css library, add start, stop, reset button panel, make knight move

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,11 @@
-const Board = ({ children }) => (
-  <table>
-    <tbody>
-      {children}
-    </tbody>
-  </table>
+const Board = ({ refCallback, children }) => (
+  <div className="board-wrapper">
+    <table ref={refCallback}>
+      <tbody>
+        {children}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default Board;
